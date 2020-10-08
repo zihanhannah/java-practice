9008de96739383f4f4aba53fab3b9de111660fba
zihan zhao <zihanhannah@gmail.com>
2020-10-07 22:31:57 -0500

    else-if

---
+++ ElseIF.java
@@ -0,0 +1,31 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+//Given an integer, , perform the following conditional actions:
+//
+//If  is odd, print Weird
+//If  is even and in the inclusive range of  to , print Not Weird
+//If  is even and in the inclusive range of  to , print Weird
+//If  is even and greater than , print Not Weird
+//Complete the stub code provided in your editor to print whether or not  is weird.
+public class ElseIF {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N % 2 == 1){
+            System.out.println("Weird");
+        } else if(N>=2 || N<=5){
+            System.out.println("Not Weird");
+        } else if(N>=6 || N <= 20){
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+        scanner.close();
+    }
+
+
+
+}
