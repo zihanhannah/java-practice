22a4c8bc759f1364995dddfd7f6d9feac6e9b5cf
zihan zhao <zihanhannah@gmail.com>
2020-10-09 21:17:35 -0500

    substring()

--- Substring.java
+++ Substring.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Substring {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String S = in.next();
+        String S = in.nextLine();
         int start = in.nextInt();
         int end = in.nextInt();
         String subString = S.substring(start,end);
