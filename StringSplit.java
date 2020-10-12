6633bee9b7aa4e6f9a1c1dfd77ece7dde1f51af4
zihan zhao <zihanhannah@gmail.com>
2020-10-11 20:41:33 -0500

    the position of scanner.nextLine()matters, scanner.hasNext()to check

--- StringSplit.java
+++ StringSplit.java
@@ -1,7 +1,34 @@
-import java.lang.reflect.Array;
+
 import java.util.Scanner;
 
-//Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
+/* Java String Token
+Given a string, find number of words in that string. For this problem a word is defined by a string of one or more
+english alphabets.
+There are multiple ways to tokenize a string in java, learn how to tokenize a string in java and demonstrate your
+skill by solving this problem!
+Input Format
+A string not more than 400000 characters long. The string can be defined by following regular expression:
+[A-Za-z !,?.\_'@]+
+That means the string will only contain english alphabets, blank spaces and this characters: "!,?._'@".
+Output Format
+In the first line, print number of words n in the string. The words don't need to be unique. In the next n lines,
+print all the words you found in the order they appeared in the input.
+Sample Input
+He is a very very good boy, isn't he?
+Sample Output
+10
+He
+is
+a
+very
+very
+good
+boy
+isn
+t
+he
+*/
+
 public class StringSplit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
